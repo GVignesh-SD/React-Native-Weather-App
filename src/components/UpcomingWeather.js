7098b3ca8475a69217d6f216ca23c6b1799e066d
GVignesh-SD <gvignesh380980@gmail.com>
2023-07-10 22:37:56 +0530

    Weather App in progress

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -44,17 +44,6 @@ const DATA = [
 ]
 
 
-const Item = (props) => {
-    const { dt_txt, min, max, condition } = props;
-    return (
-        <View style={styles.item}>
-            <Feather name={'sun'} size={50} color={'white'} />
-            <Text style={styles.date}>{dt_txt}</Text>
-            <Text style={styles.temp}>{min}</Text>
-            <Text style={styles.temp}>{max}</Text>
-        </View>
-    );
-}
 
 
 const UpcomingWeather = () => {
